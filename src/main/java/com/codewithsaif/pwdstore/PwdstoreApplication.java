42c45e5537bca5e2cd722e179183659d7adb9047
shaikh Saifuddin <u4saif@gmail.com>
2025-08-21 13:41:16 +0530

    Initial commit with home controller using spring-web

---
+++ src/main/java/com/codewithsaif/pwdstore/PwdstoreApplication.java
@@ -0,0 +1,13 @@
+package com.codewithsaif.pwdstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwdstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwdstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithsaif/pwdstore/PwdstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithsaif.pwdstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwdstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
